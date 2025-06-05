2082d52a17dcba6c03ff6c2810daa528f2dc2878
sarbudragos <dragossarbu2003@gmail.com>
2025-06-05 12:58:10 +0300

    changed date formatting

--- axum-social-react/src/components/post/PostCard.tsx
+++ axum-social-react/src/components/post/PostCard.tsx
@@ -2,6 +2,11 @@ import { Card, CardContent, Typography } from "@mui/material"
 import type { Post } from "../../model/Post"
 
 function PostCard({ post }: { post: Post }) {
+
+  const index_of_T = post.creation_date.indexOf('T');
+  const truncated_creation_date = post.creation_date.substring(0, index_of_T);
+
+  console.log(truncated_creation_date);
   
   return (
     <Card>
@@ -10,7 +15,7 @@ function PostCard({ post }: { post: Post }) {
                 {post.content}
             </Typography>
             <Typography sx={{ color: 'text.secondary'} } variant="h6">
-                Posted on: {post.creation_date}
+                Posted on: {truncated_creation_date}
             </Typography>
         </CardContent>
     </Card>
